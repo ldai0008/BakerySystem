9fb9271958dc8d20a84044b43418c7090f98570d
Bella <ldai0008@student.monash.edu>
2020-10-26 21:43:23 +0800

    modify the account using the name and password

--- src/com/bakery/utils/UserUtils.java
+++ src/com/bakery/utils/UserUtils.java
@@ -17,7 +17,8 @@ public class UserUtils {
         List<String> users = FileUtils.readFile("user.csv");
         for (String user : users) {
             String[] u = user.split(",");
-            if ((Integer.parseInt(u[0]) == Integer.parseInt(account) || u[2].equals(account))
+//            if ((Integer.parseInt(u[1]) == Integer.parseInt(account) || u[2].equals(account))
+            if ((u[1].toLowerCase().equals(account.toLowerCase()) || u[2].equals(account))
                     && u[3].equals(password)) {
                 int userId = Integer.parseInt(u[0]);
                 User aUser = new User(userId, u[1], u[2], u[3], u[4], u[5], u[6], u[7], u[8]);
