88b35fa63e871e70e6ebd73bba4f63bd4d7d9ab0
Bella <ldai0008@student.monash.edu>
2020-10-31 17:00:13 +0800

    commit for Report

--- src/Report.java
+++ src/Report.java
@@ -8,7 +8,10 @@ public class Report {
 	private Store store;
 
 
-
+	/**
+	 * Constructor for objects of class Report
+	 * have a parameterised constructor
+	 */
 	public Report(LocalDate dateOfReport, String nameOfReport, String typeOfReport, Store store) {
 		super();
 		this.dateOfReport = dateOfReport;
@@ -17,34 +20,66 @@ public class Report {
 		this.store = store;
 	}
 
+	/**
+	 * Get method
+	 * @return the dateOfReport
+	 */
 	public LocalDate getDateOfReport() {
 		return dateOfReport;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   dateOfReport   the new date of report in the Report
+	 */
 	public void setDateOfReport(LocalDate dateOfReport) {
 		this.dateOfReport = dateOfReport;
 	}
 
+	/**
+	 * Get method
+	 * @return the nameOfReport
+	 */
 	public String getNameOfReport() {
 		return nameOfReport;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   nameOfReport   the new nameOfReport in the Report
+	 */
 	public void setNameOfReport(String nameOfReport) {
 		this.nameOfReport = nameOfReport;
 	}
 
+	/**
+	 * Get method
+	 * @return the typeOfReport
+	 */
 	public String getTypeOfReport() {
 		return typeOfReport;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   typeOfReport   the new type of report in the Report
+	 */
 	public void setTypeOfReport(String typeOfReport) {
 		this.typeOfReport = typeOfReport;
 	}
 
+	/**
+	 * Get method
+	 * @return the store
+	 */
 	public Store getStore() {
 		return store;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   store   the new store in the Report
+	 */
 	public void setStore(Store store) {
 		this.store = store;
 	}
