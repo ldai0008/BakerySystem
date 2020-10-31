49383fb6a70a37541c51052570d8ae9ea9e4cc7e
Bella <ldai0008@student.monash.edu>
2020-10-31 16:33:13 +0800

    commit for Bakery

--- src/Bakery.java
+++ src/Bakery.java
@@ -4,18 +4,34 @@ import java.util.List;
 public class Bakery {
 
 	private ArrayList<Store> listOfStore;
-
+	/**
+	 * Constructor for objects of class Bakery
+	 * have a non-parameterised (“default”) constructor
+	 */
 	public Bakery() {
 		this.listOfStore = new ArrayList<>();
 	}
+
+	/**
+	 * Constructor for objects of class Bakery
+	 * have a parameterised constructor
+	 */
 	public Bakery(ArrayList<Store> listOfStore) {
 		this.listOfStore = listOfStore;
 	}
 
+	/**
+	 * Get method
+	 * @return the listOfStore
+	 */
 	public ArrayList<Store> getListOfStore() {
 		return listOfStore;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   listOfStore  the new listOfStore in the bakery
+	 */
 	public void setListOfStore(ArrayList<Store> listOfStore) {
 		this.listOfStore = listOfStore;
 	}
