548995ed08fb420bf37e8f9a03397844b407d7f1
Bella <ldai0008@student.monash.edu>
2020-10-31 16:54:32 +0800

    commit for Order

--- src/Order.java
+++ src/Order.java
@@ -14,6 +14,10 @@ public class Order {
 	private String nameOfCustomer;
 	private String lastModifiedBy;
 
+	/**
+	 * Constructor for objects of class Order
+	 * have a non-parameterised (“default”) constructor
+	 */
 	public Order() {
 		this.orderDate = "";
 		this.orderTime = "";
@@ -26,6 +30,10 @@ public class Order {
 		this.lastModifiedBy = "";
 	}
 
+	/**
+	 * Constructor for objects of class Order
+	 * have a parameterised constructor
+	 */
 	public Order(String orderDate, String orderId, String orderTime, String orderStatus,
 			Map<FoodItem, Integer> quantity, Double totalCost, String lastModifiedDate, String lastModifiedTime,
 			String nameOfCustomer, String lastModifiedBy) {
@@ -40,86 +48,171 @@ public class Order {
 		this.nameOfCustomer = nameOfCustomer;
 		this.lastModifiedBy = lastModifiedBy;
 	}
+
+	/**
+	 * Get method
+	 * @return the “default”
+	 */
 	public String getCustomerPhone(){
 		return "";
 	}
 
+	/**
+	 * Get method
+	 * @return the orderDate
+	 */
 	public String getOrderDate() {
 		return orderDate;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   orderDate   the order data in the Order
+	 */
 	public void setOrderDate(String orderDate) {
 		this.orderDate = orderDate;
 	}
 
+	/**
+	 * Get method
+	 * @return the orderId
+	 */
 	public String getOrderId() {
 		return orderId;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   orderId   the new orderId in the Order
+	 */
 	public void setOrderId(String orderId) {
 		this.orderId = orderId;
 	}
 
+	/**
+	 * Get method
+	 * @return the orderTime
+	 */
 	public String getOrderTime() {
 		return orderTime;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   orderTime   the new order time in the Order
+	 */
 	public void setOrderTime(String orderTime) {
 		this.orderTime = orderTime;
 	}
 
+	/**
+	 * Get method
+	 * @return the orderStatus
+	 */
 	public String getOrderStatus() {
 		return orderStatus;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   orderStatus   the new orderStatus in the Order
+	 */
 	public void setOrderStatus(String orderStatus) {
 		this.orderStatus = orderStatus;
 	}
 
+	/**
+	 * Get method
+	 * @return the quantity
+	 */
 	public Map<FoodItem, Integer> getQuantity() {
 		return quantity;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   quantity   the new quantity in the Order
+	 */
 	public void setQuantity(Map<FoodItem, Integer> quantity) {
 		this.quantity = quantity;
 	}
 
+	/**
+	 * Get method
+	 * @return the totalCost
+	 */
 	public Double getTotalCost() {
 		return totalCost;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   totalCost   the new totalCost in the Order
+	 */
 	public void setTotalCost(Double totalCost) {
 		this.totalCost = totalCost;
 	}
 
+	/**
+	 * Get method
+	 * @return the lastModifiedDate
+	 */
 	public String getLastModifiedDate() {
 		return lastModifiedDate;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   lastModifiedDate   the new lastModifiedDate in the Order
+	 */
 	public void setLastModifiedDate(String lastModifiedDate) {
 		this.lastModifiedDate = lastModifiedDate;
 	}
 
+	/**
+	 * Get method
+	 * @return the lastModifiedTime
+	 */
 	public String getLastModifiedTime() {
 		return lastModifiedTime;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   lastModifiedTime   the new lastModifiedTime in the Order
+	 */
 	public void setLastModifiedTime(String lastModifiedTime) {
 		this.lastModifiedTime = lastModifiedTime;
 	}
 
+	/**
+	 * Get method
+	 * @return the nameOfCustomer
+	 */
 	public String getNameOfCustomer() {
 		return nameOfCustomer;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   nameOfCustomer   the new nameOfCustomer in the Order
+	 */
 	public void setNameOfCustomer(String nameOfCustomer) {
 		this.nameOfCustomer = nameOfCustomer;
 	}
 
+	/**
+	 * Get method
+	 * @return the lastModifiedBy
+	 */
 	public String getLastModifiedBy() {
 		return lastModifiedBy;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   lastModifiedBy   the new last modify in the Order
+	 */
 	public void setLastModifiedBy(String lastModifiedBy) {
 		this.lastModifiedBy = lastModifiedBy;
 	}
