1ef2640be88efa4fe011c6c3ba2c42181c462d9b
Bella <ldai0008@student.monash.edu>
2020-10-31 17:06:02 +0800

    commit for Store

--- src/Store.java
+++ src/Store.java
@@ -10,6 +10,10 @@ public class Store {
 	private List<Order> listOfOrder;
 	private List<Inventory> listOfInventory;
 
+	/**
+	 * Constructor for objects of class Store
+	 * have a non-parameterised (“default”) constructor
+	 */
 	public Store() {
 		super();
 		storeId = "";
@@ -20,6 +24,10 @@ public class Store {
 		listOfInventory = new ArrayList<>();
 	}
 
+	/**
+	 * Constructor for objects of class Store
+	 * have a parameterised constructor
+	 */
 	public Store(String storeId, String storeAddress, String storeContactNumber, List<User> listOfUser,
 			List<Order> listOfOrder, List<Inventory> listOfInventory) {
 		super();
@@ -31,50 +39,98 @@ public class Store {
 		this.listOfInventory = listOfInventory;
 	}
 
+	/**
+	 * Get method
+	 * @return the storeId
+	 */
 	public String getStoreId() {
 		return storeId;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   storeId   the new store ID in the Store
+	 */
 	public void setStoreId(String storeId) {
 		this.storeId = storeId;
 	}
 
+	/**
+	 * Get method
+	 * @return the storeAddress
+	 */
 	public String getStoreAddress() {
 		return storeAddress;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   storeAddress   the new storeAddress in the Store
+	 */
 	public void setStoreAddress(String storeAddress) {
 		this.storeAddress = storeAddress;
 	}
 
+	/**
+	 * Get method
+	 * @return the storeContactNumber
+	 */
 	public String getStoreContactNumber() {
 		return storeContactNumber;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   storeContactNumber   the new storeContactNumber in the Store
+	 */
 	public void setStoreContactNumber(String storeContactNumber) {
 		this.storeContactNumber = storeContactNumber;
 	}
 
+	/**
+	 * Get method
+	 * @return the listOfUser
+	 */
 	public List<User> getListOfUser() {
 		return listOfUser;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   listOfUser   the new list of user in the Store
+	 */
 	public void setListOfUser(List<User> listOfUser) {
 		this.listOfUser = listOfUser;
 	}
 
+	/**
+	 * Get method
+	 * @return the listOfOrder
+	 */
 	public List<Order> getListOfOrder() {
 		return listOfOrder;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   listOfOrder   the new list of order in the Store
+	 */
 	public void setListOfOrder(List<Order> listOfOrder) {
 		this.listOfOrder = listOfOrder;
 	}
 
+	/**
+	 * Get method
+	 * @return the listOfInventory
+	 */
 	public List<Inventory> getListOfInventory() {
 		return listOfInventory;
 	}
 
+	/**
+	 * Mutator method
+	 * @param   listOfInventory   the new list of inventory in the Store
+	 */
 	public void setListOfInventory(List<Inventory> listOfInventory) {
 		this.listOfInventory = listOfInventory;
 	}
